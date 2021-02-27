0de1556c07cf848c31538520a1f8d21b9c2e992d
lesenelir <miaomiaobabyzy@gmail.com>
2021-02-27 17:12:15 +0800

    commit wrapper basic important

--- src/com/Demo07_WrapperClass/WrapperTest01.java
+++ src/com/Demo07_WrapperClass/WrapperTest01.java
@@ -28,7 +28,7 @@ public class WrapperTest01 {
         System.out.println(t4);
         System.out.println(t5);
 
-        double t6 = t2.doubleValue();  // 【拆箱多用于转换成不同的数据类型的表示】
+        double t6 = t2.doubleValue();  // 【手动拆箱调用xxxValue()多用于转换成不同的数据类型的表示】
         System.out.println(t6);
 
 
