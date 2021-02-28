e8e80e5ba8145e2307c0642e8070ced2a8d3fb39
lesenelir <miaomiaobabyzy@gmail.com>
2021-02-28 20:36:34 +0800

    commit update HashSet demo projects

---
+++ src/com/Demo09_gather/LIst/Hw02/Student.java
@@ -0,0 +1,70 @@
+package com.Demo09_gather.LIst.Hw02;
+
+import java.util.Objects;
+
+public class Student {
+    private int stuId;
+    private String name;
+    private float score;
+
+    // constructor
+    public Student() {
+    }
+
+    public Student(int stuId, String name, float score) {
+        this.stuId = stuId;
+        this.name = name;
+        this.score = score;
+    }
+
+    // getter | setter
+    public int getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(int stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    // tostring方法重写，显示的时候直接显示出来
+    @Override
+    public String toString() {
+        return "[" +
+                "学号：" + stuId +
+                ", 姓名：'" + name + '\'' +
+                ", 成绩：" + score +
+                ']';
+    }
+
+    // 重写hashcode方法和equals方法
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return stuId == student.stuId &&
+//                Float.compare(student.score, score) == 0 &&
+                name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stuId, name, score);
+    }
+}
---
+++ src/com/Demo09_gather/LIst/Hw02/StudentTest.java
@@ -0,0 +1,38 @@
+package com.Demo09_gather.LIst.Hw02;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        // 定义三个Student类的对象
+        Student one = new Student(3, "William", 65);
+        Student two = new Student(1, "Tom", 87);
+        Student three = new Student(2, "Lucy", 95);
+
+        // 定义HashSet
+        Set set = new HashSet();
+        set.add(one);
+        set.add(two);
+        set.add(three);
+
+        // 迭代器遍历HashSet结果
+        Iterator it = set.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+
+        System.out.println("===============");
+        // 添加相同的元素   【要重写hashcode和equals方法才能不添加相同的元素】
+        Student four = new Student(3, "William", 65);
+        set.add(four);
+        it = set.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+
+    }
+}
