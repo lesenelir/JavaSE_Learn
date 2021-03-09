30031cc0778072d11ce6b054f1b087e93aae1656
lesenelir <miaomiaobabyzy@gmail.com>
2021-03-09 15:31:46 +0800

    update

--- src/java_Algorithm/Demo01_Array/Demo08_18/Test_18.java
+++ src/java_Algorithm/Demo01_Array/Demo08_18/Test_18.java
@@ -14,7 +14,7 @@ public class Test_18 {
         for (int i = 0; i < nums.length; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
                 int k = j + 1, l = nums.length - 1;
@@ -45,7 +45,7 @@ public class Test_18 {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[] {1,0,-1,0,-2,2};
+        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
         List<List<Integer>> lists = fourSum(nums, 0);
         System.out.println(lists);
     }
